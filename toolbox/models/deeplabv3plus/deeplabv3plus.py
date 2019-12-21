b6da92fbf7dcff2f540e45130a9a5312c3245885
dtrimina <804306293@qq.com>
2019-12-21 11:25:10 +0800

    add deeplabv3plus

---
+++ toolbox/models/deeplabv3plus/deeplabv3plus.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+from toolbox.models.deeplabv3plus.resnet import resnet101_atrous, resnet50_atrous
+
+
+class ConvBNReLU(nn.Module):
+    def __init__(self, in_chan, out_chan, ks=3, stride=1, padding=1, dilation=1, *args, **kwargs):
+        super(ConvBNReLU, self).__init__()
+        self.conv = nn.Conv2d(in_chan, out_chan, kernel_size=ks, stride=stride,
+                              padding=padding, dilation=dilation, bias=True)
+        self.bn = nn.BatchNorm2d(out_chan)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        return self.relu(x)
+
+
+class ASPP(nn.Module):
+    def __init__(self, in_chan=2048, out_chan=256, with_gp=True, *args, **kwargs):
+        super(ASPP, self).__init__()
+        self.with_gp = with_gp
+        self.conv1 = ConvBNReLU(in_chan, out_chan, ks=1, dilation=1, padding=0)
+        self.conv2 = ConvBNReLU(in_chan, out_chan, ks=3, dilation=6, padding=6)
+        self.conv3 = ConvBNReLU(in_chan, out_chan, ks=3, dilation=12, padding=12)
+        self.conv4 = ConvBNReLU(in_chan, out_chan, ks=3, dilation=18, padding=18)
+        if self.with_gp:
+            self.avg = nn.AdaptiveAvgPool2d((1, 1))
+            self.conv1x1 = ConvBNReLU(in_chan, out_chan, ks=1)
+            self.conv_out = ConvBNReLU(out_chan * 5, out_chan, ks=1)
+        else:
+            self.conv_out = ConvBNReLU(out_chan * 4, out_chan, ks=1)
+
+    def forward(self, x):
+        H, W = x.size()[2:]
+        feat1 = self.conv1(x)
+        feat2 = self.conv2(x)
+        feat3 = self.conv3(x)
+        feat4 = self.conv4(x)
+        if self.with_gp:
+            avg = self.avg(x)
+            feat5 = self.conv1x1(avg)
+            feat5 = F.interpolate(feat5, (H, W), mode='bilinear', align_corners=True)
+            feat = torch.cat([feat1, feat2, feat3, feat4, feat5], 1)
+        else:
+            feat = torch.cat([feat1, feat2, feat3, feat4], 1)
+        feat = self.conv_out(feat)
+        return feat
+
+
+class Decoder(nn.Module):
+    def __init__(self, n_classes, low_chan=256, *args, **kwargs):
+        super(Decoder, self).__init__()
+        self.conv_low = ConvBNReLU(low_chan, 48, ks=1, padding=0)
+        self.conv_cat = nn.Sequential(
+            ConvBNReLU(304, 256, ks=3, padding=1),
+            ConvBNReLU(256, 256, ks=3, padding=1),
+        )
+        self.conv_out = nn.Conv2d(256, n_classes, kernel_size=1, bias=False)
+
+    def forward(self, feat_low, feat_aspp):
+        H, W = feat_low.size()[2:]
+        feat_low = self.conv_low(feat_low)
+        feat_aspp_up = F.interpolate(feat_aspp, (H, W), mode='bilinear', align_corners=True)
+        feat_cat = torch.cat([feat_low, feat_aspp_up], dim=1)
+        feat_out = self.conv_cat(feat_cat)
+        logits = self.conv_out(feat_out)
+        return logits
+
+
+class Deeplab_v3plus(nn.Module):
+    def __init__(self, n_classes, os=16, aspp_global_feature=True, backbone='resnet50', *args, **kwargs):
+        super(Deeplab_v3plus, self).__init__()
+        if backbone == 'resnet50':
+            self.backbone = resnet50_atrous(pretrained=True, os=os)
+        if backbone == 'resnet101':
+            self.backbone = resnet101_atrous(pretrained=True, os=os)
+
+        self.backbone = resnet101_atrous(pretrained=True, os=16)
+        self.aspp = ASPP(in_chan=2048, out_chan=256, with_gp=aspp_global_feature)
+        self.decoder = Decoder(n_classes, low_chan=256)
+
+    def forward(self, x):
+        H, W = x.size()[2:]
+        feat4, _, _, feat32 = self.backbone(x)
+        feat_aspp = self.aspp(feat32)
+        logits = self.decoder(feat4, feat_aspp)
+        logits = F.interpolate(logits, (H, W), mode='bilinear', align_corners=True)
+
+        return logits
+
+
+if __name__ == "__main__":
+    import torch
+
+    # model = resnet50_atrous(pretrained=True)
+    model = Deeplab_v3plus(n_classes=41)
+    input = torch.randn((2, 3, 512, 1024))
+    output = model(input)
+    print(output.size())
---
+++ toolbox/models/deeplabv3plus/resnet.py
@@ -0,0 +1,178 @@
+import torch.nn as nn
+import math
+import torch.utils.model_zoo as model_zoo
+
+model_urls = {
+    'resnet18': 'https://download.pytorch.org/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://download.pytorch.org/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://download.pytorch.org/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://download.pytorch.org/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://download.pytorch.org/models/resnet152-b121ed2d.pth',
+}
+
+
+def conv3x3(in_planes, out_planes, stride=1, atrous=1):
+    """3x3 convolution with padding"""
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1 * atrous, dilation=atrous, bias=False)
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, inplanes, planes, stride=1, atrous=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(inplanes, planes, stride, atrous)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.downsample is not None:
+            residual = self.downsample(x)
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, stride=1, atrous=1, downsample=None):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
+                               padding=1 * atrous, dilation=atrous, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, planes * self.expansion, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class ResNet_Atrous(nn.Module):
+
+    def __init__(self, block, layers, atrous=None, os=16):
+        super(ResNet_Atrous, self).__init__()
+        if os == 8:
+            stride_list = [2, 1, 1]
+        elif os == 16:
+            stride_list = [2, 2, 1]
+        else:
+            raise ValueError('resnet_atrous.py: output stride=%d is not supported.' % os)
+
+        self.inplanes = 64
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, 64, layers[0])
+        self.layer2 = self._make_layer(block, 256, 128, layers[1], stride=stride_list[0])
+        self.layer3 = self._make_layer(block, 512, 256, layers[2], stride=stride_list[1], atrous=16 // os)
+        self.layer4 = self._make_layer(block, 1024, 512, layers[3], stride=stride_list[2],
+                                       atrous=[item * 16 // os for item in atrous])
+
+    def _make_layer(self, block, inplanes, planes, blocks, stride=1, atrous=None):
+        downsample = None
+        if atrous == None:
+            atrous = [1] * blocks
+        elif isinstance(atrous, int):
+            atrous_list = [atrous] * blocks
+            atrous = atrous_list
+        if stride != 1 or inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(inplanes, planes * block.expansion, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion), )
+
+        layers = []
+        layers.append(block(inplanes, planes, stride=stride, atrous=atrous[0], downsample=downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(planes * block.expansion, planes, stride=1, atrous=atrous[i]))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        features = []
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        features.append(x)
+        x = self.layer2(x)
+        features.append(x)
+        x = self.layer3(x)
+        features.append(x)
+        x = self.layer4(x)
+        features.append(x)
+
+        return features
+
+
+def resnet50_atrous(pretrained=True, os=16, **kwargs):
+    """Constructs a atrous ResNet-50 model."""
+    model = ResNet_Atrous(Bottleneck, [3, 4, 6, 3], atrous=[1, 2, 1], os=os, **kwargs)
+    if pretrained:
+        old_dict = model_zoo.load_url(model_urls['resnet50'])
+        model_dict = model.state_dict()
+        old_dict = {k: v for k, v in old_dict.items() if (k in model_dict)}
+        model_dict.update(old_dict)
+        model.load_state_dict(model_dict)
+    return model
+
+
+def resnet101_atrous(pretrained=True, os=16, **kwargs):
+    """Constructs a atrous ResNet-101 model."""
+    model = ResNet_Atrous(Bottleneck, [3, 4, 23, 3], atrous=[2, 2, 2], os=os, **kwargs)
+    if pretrained:
+        old_dict = model_zoo.load_url(model_urls['resnet101'])
+        model_dict = model.state_dict()
+        old_dict = {k: v for k, v in old_dict.items() if (k in model_dict)}
+        model_dict.update(old_dict)
+        model.load_state_dict(model_dict)
+    return model
+
+
+if __name__ == '__main__':
+    import torch
+
+    # model = resnet50_atrous(pretrained=True)
+    model = resnet101_atrous(pretrained=True)
+    input = torch.randn((2, 3, 512, 1024))
+    features = model(input)
+
+    for fea in features:
+        print(fea.size())
